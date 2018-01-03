712494fc00d1786294a79d46eb697d304881a967
WangXiaoguang0712 <446889194@qq.com>
2018-01-03 23:34:11 +0800

    viterbi

---
+++ twi/hmm/viterbi.py
@@ -0,0 +1,46 @@
+__author__ = 'T'
+
+states = ('Healthy','Fever')
+# start_probability = {'Healthy': 0.6, 'Fever': 0.4}
+# transition_probability = {'Healthy': {'Healthy': 0.7, 'Fever': 0.3}, 'Fever': {'Healthy': 0.4, 'Fever': 0.6}}
+# emission_probability = {'Healthy': {'normal': 0.5, 'cold': 0.4, 'dizzy': 0.1},
+#                       'Fever': {'normal': 0.1, 'cold': 0.3, 'dizzy': 0.6}}
+
+start_probability = {'Healthy': 0.5, 'Fever': 0.5} # 0.6 0.4
+transition_probability = {'Healthy': {'Healthy': 0.6, 'Fever': 0.4}, 'Fever': {'Healthy': 0.4, 'Fever': 0.6}}
+emission_probability = {'Healthy': {'normal': 0.7, 'cold': 0.2, 'dizzy': 0.1},
+                      'Fever': {'normal': 0.1, 'cold': 0.3, 'dizzy': 0.6}}
+
+
+def viterbit(obs, states, s_pro, t_pro, e_pro):
+    path = { s:[] for s in states} # init path: path[s] represents the path ends with s
+    curr_pro = {}
+    for s in states:
+        curr_pro[s] = s_pro[s] * e_pro[s][obs[0]]
+    for i in xrange(1, len(obs)):
+        last_pro = curr_pro
+        curr_pro = {}
+        for curr_state in states:
+            max_pro, last_sta = max(((last_pro[last_state] * t_pro[last_state][curr_state] * e_pro[curr_state][obs[i]],
+                                      last_state) for last_state in states))
+            curr_pro[curr_state] = max_pro
+            path[curr_state].append(last_sta)
+
+    # find the final largest probability
+    max_pro = -1
+    max_path = None
+    for s in states:
+        path[s].append(s)
+        if curr_pro[s] > max_pro:
+            max_path = path[s]
+            max_pro = curr_pro[s]
+    # print '%s: %s'%(curr_pro[s], path[s]) # different path and their probability
+    return max_path
+
+
+def viterbi(obs, states, s_pro, t_pro, e_pro):
+    pass
+
+if __name__ == '__main__':
+	obs = ['normal', 'cold', 'dizzy']
+	print viterbit(obs, states, start_probability, transition_probability, emission_probability)
